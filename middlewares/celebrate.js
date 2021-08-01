42e344688a544943300b9a26fabdcbb60a620bac
vsmeyukha <vsmeyukha@gmail.com>
2021-08-02 00:24:51 +0300

    исправлено: пользователь без имени больше не может зарегистрироваться

--- middlewares/celebrate.js
+++ middlewares/celebrate.js
@@ -16,7 +16,7 @@ const validateUserId = (id, helpers) => {
 };
 
 const userInfo = {
-  name: Joi.string().min(2).max(30),
+  name: Joi.string().required().min(2).max(30),
   email: Joi.string().required().email(),
 };
 
